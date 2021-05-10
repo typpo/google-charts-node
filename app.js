c7a03606dd358bf0faba752175d7b06fdd8f878b
Ian Webster <ianw_github@ianww.com>
2021-05-10 10:51:28 -0700

    Improve app example

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const { renderHttp } = require('./index');
 
 const app = express();
+app.use(express.json());
 
 app.get('/google-charts', renderHttp);
+app.post('/google-charts', renderHttp);
 
 const port = process.env.PORT || 3500;
 app.listen(port);
